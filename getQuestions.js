4bdef4fb605941e24445908e0b092db75ca0ad49
ryj7 <ryj7@aber.ac.uk>
2024-05-25 00:46:21 +0100

    Added headers and footers to html and made some improvements to the function that fetches the questions.

--- getQuestions.js
+++ getQuestions.js
@@ -1,4 +1,35 @@
-function getQuestions() {
+async function setQuiz() {
+    const questionsResponse = await getQuestions();
+    const questions = questionsResponse;
+    displayQuestions(questions.results);
+}
+
+//switch player
+//display new question
+//add score
+//when question all questions are done, finsih game
+
+function displayQuestions(questions) {
+    console.log(questions);
+    questions.forEach(question => {
+        document.getElementById("questions").innerHTML = question.question;
+    });
+}
+
+async function getQuestions() {
+
+    const quizUrl = 'https://opentdb.com/api.php?';
+
+    const parameters = {
+        amount: 40
+    };
+
+    const paramString = Object.keys(parameters)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(parameters[key])}`)
+        .join('&');
+
+    const newUrl = `${quizUrl}${paramString}`;
+
     const myHeaders = new Headers();
     myHeaders.append("Cookie", "PHPSESSID=618f0aae0d60308137b17578ebd50bf7");
 
@@ -8,9 +39,11 @@ function getQuestions() {
         redirect: "follow"
     };
 
-    fetch("https://opentdb.com/api.php?amount=20", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.table(result))
-        .catch((error) => console.error(error));
-
+    try {
+        const response = await fetch(newUrl, requestOptions);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        throw new Error('Error fetching questions:', error);
+    }
 }
