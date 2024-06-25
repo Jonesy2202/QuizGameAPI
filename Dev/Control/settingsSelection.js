658f11f4e0cbc8785a819d84105ae76561f4ec86
ryj7 <ryj7@aber.ac.uk>
2024-06-25 18:04:49 +0100

    navigation tabs work on the card that displays the players and settings. can now switch between them.

---
+++ Dev/Control/settingsSelection.js
@@ -0,0 +1,14 @@
+
+function displayPlayersTab() {
+    document.getElementById("quizSettings").style.display = "none";
+    document.getElementById("playerForm").style.display = "block";
+    document.getElementById("addPlayersTab").classList.add("active");
+    document.getElementById("gameSettingsTab").classList.remove("active");
+}
+
+function displaySettingsTab() {
+    document.getElementById("quizSettings").style.display = "block";
+    document.getElementById("playerForm").style.display = "none";
+    document.getElementById("gameSettingsTab").classList.add("active");
+    document.getElementById("addPlayersTab").classList.remove("active");
+}
