db66d2e255e1376b917ad88bd7b15fe8c85c8a2e
Jonesy2202 <157637744+Jonesy2202@users.noreply.github.com>
2024-07-31 22:23:30 +0100

    finished alert

--- Dev/Control/quizSettings.js
+++ Dev/Control/quizSettings.js
@@ -4,18 +4,26 @@ function quizSettings() {
     let category = document.getElementById("category").value;
     let difficulty = document.getElementById("difficultySlider").value;
 
-    if(difficulty == 1){
+    if (difficulty == 1) {
         quizParameters.difficulty = "easy";
-    } else if(difficulty == 2){
+    } else if (difficulty == 2) {
         quizParameters.difficulty = "medium";
-    } else if(difficulty == 3){
+    } else if (difficulty == 3) {
         quizParameters.difficulty = "hard";
     } else {
         quizParameters.difficulty = "medium";
     }
 
-        quizParameters.category = category;
+    quizParameters.category = category;
 
-        document.getElementById("confirmSettings").style.display = "block";
-        document.getElementById("confirmSettings").innerHTML = "Settings have been changed.";
+    document.getElementById("confirmSettings").style.display = "block";
+
+    if (!$("#confirmSettings").children().length) {
+        $("#confirmSettings")
+            .append("<span class='closebtn'>&times;</span>")
+            .find(".closebtn").click(function () {
+                $(this).parent().hide();
+            });
+        $("#confirmSettings").append("Quiz settings changed successfully.");
+    }
 }
